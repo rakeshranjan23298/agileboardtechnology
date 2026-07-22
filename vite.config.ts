10b6309a8458967708ce56ed7b4042ff17d853f3
rakeshRanjan <git@123>
2026-07-22 10:42:43 +0530

    image sequence change

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [react(), tailwindcss(), tsconfigPaths(), cloudflare()],
   server: {
     host: true,
     port: 3000,
