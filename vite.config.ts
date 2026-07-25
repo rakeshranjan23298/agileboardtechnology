27866aca521031dfccc94de6d71a4cf85e947edf
rakeshRanjan <git@123>
2026-07-25 17:34:24 +0530

    fix: remove cloudflare plugin from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths(), cloudflare()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   server: {
     host: true,
     port: 3000,
